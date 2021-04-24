14c7e7e96f9520182195cd4999e5166d6c86fe78
himani412 <77782996+himani412@users.noreply.github.com>
2021-04-24 08:45:19 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-studentArray=[]
-function submit(){
-var displaystudent =[] 
-for (j=1; j<= 4 ; j++){
-    var namestudent=document.getElementById("name_of_the_student_"+j).value;
-    console.log(namestudent);
-    studentArray.push(namestudent);
-}
- console.log(studentArray)
- var lengtharray=studentArray.length;
- console.log(lengtharray);
- 
- for (k=0; k<lengtharray;k++){
-     displaystudent.push("<h4>NAME - "+studentArray[k]+"</h4>");
-     console.log(displaystudent);
- }
- console.log(displaystudent); document.getElementById("display_name_with_commas").innerHTML = displaystudent; var remove_commas = displaystudent.join(" "); console.log(remove_commas); document.getElementById("display_name_without_commas").innerHTML = remove_commas; document.getElementById("submit_button").style.display = "none"; document.getElementById("sort_button").style.display = "inline-block";
-
-   
- }
- function sorting() { studentArray.sort(); console.log(studentArray); var display_student_array_sorting = []; var lengtharray = studentArray.length; console.log(lengtharray); for (var k = 0; k < lengtharray; k++) { display_student_array_sorting.push("<h4>NAME - " + studentArray[k] + "</h4>"); console.log(display_student_array_sorting); } var remove_commas = display_student_array_sorting.join(" "); console.log(remove_commas); document.getElementById("display_name_without_commas").innerHTML = remove_commas; }
-
+var canvas=new fabric.Canvas('myCanvas');
+blockwidth=30;
+blockheight=30;
+playerx=10;
+playery=10;
+var playerobject="";
+function playerupdate(){
+    fabric.Image.fromURL("player.png",function (Img){
+        playerobject=Img;
+       playerobject.scaleToWidth(150);
+        playerobject.scaleToHeight(140);
+         playerobject.set({ top:playery, left:playerx });
+          canvas.add(playerobject); }); }
+  function newimage(getimage){
+ fabric.Image.fromURL(getimage,function (Img){
+      blockobject=Img;
+     blockobject.scaleToWidth(blockwidth);
+  blockobject.scaleToHeight(blockheight);
+  blockobject.set({ top:playery, left:playerx });
+      canvas.add(blockobject); }); }
+    
