c72816a791d3d97b699be70615488a4c51ca085d
himani412 <77782996+himani412@users.noreply.github.com>
2021-07-01 07:38:38 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,35 +1,7 @@
-Webcam.set({
-    width:310,
-    height:300,
-    Imageformat:'png',
-    pngquality:90,
-
-    constraints:{
-        facingMode:"environment"
-    }
-});
-camera=document.getElementById("camera");
-Webcam.attach('#camera');
-function takesnapshot(){
-    Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML='<img id="capturedimage" src="'+data_uri+'"/>';
-    });
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
 }
-console.log('ml5 version:', ml5.version);
-classifier=ml5.imageClassifier('MobileNet',modelLoaded);
-function modelLoaded(){
-    console.log('Model Loaded');
-}
-function check(){
-    img=document.getElementById('capturedimage');
-    classifier.classify(img,gotresult);
-}
-function gotresult(error,results){
-    if(error){
-        console.error(error);
-    }
-    else{
-        console.log(results);
-        document.getElementById("objectname").innerHTML=results[0].label;
-    }
+function draw(){
+    
 }
